db230145785e026f4e41bcba1a17c6eba65a1303
Talvane Augusto <talvane.augusto@tupiconsultoria.com.br>
2020-04-06 10:34:10 -0300

    adicionado validacao post via celebrate

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const CodenationController = require('../src/controllers/CodenationController');
 
 const routes = express.Router();
 
-routes.post('/proces', CodenationController.proces);
+routes.post('/proces', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        token: Joi.string().required().length(40),
+    })
+}), CodenationController.proces);
 
 module.exports = routes;
